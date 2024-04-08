d2e3de58f46d391adfacc7a57f345920fb324a56
Drew Chase (Work) <dcmanproductions@gmail.com>
2024-04-08 11:53:05 -0400

    Add output feedback and error handling in GitManager
    The changes provide live feedback by outputting command results to console in each GitManager method. A message parsing with optional date and time variables was added. In addition, a catch block was introduced in the run method to handle exceptions and output corresponding error messages.

--- src/git_manager.cpp
+++ src/git_manager.cpp
@@ -4,6 +4,7 @@
 
 #include "../includes/git_manager.h"
 
+#include <iostream>
 #include <memory>
 #include <stdexcept>
 #include <thread>
@@ -14,6 +15,7 @@ namespace ScheduledGitCommit
     {
         if (std::string output; run_command("git ", output) == 0)
         {
+            std::cout << output << std::endl;
             return !output.empty();
         }
 
@@ -24,24 +26,28 @@ namespace ScheduledGitCommit
     {
         std::string output;
         run_command("git add .", output);
+        std::cout << output << std::endl;
     }
 
     void GitManager::commit() const
     {
         std::string output;
         run_command("git commit -m \"" + message + "\"", output);
+        std::cout << output << std::endl;
     }
 
     void GitManager::create_tag() const
     {
         std::string output;
         run_command("git tag -a \"" + message + "\" -m \"" + message + "\"", output);
+        std::cout << output << std::endl;
     }
 
     void GitManager::push() const
     {
         std::string output;
         run_command("git push --force", output);
+        std::cout << output << std::endl;
     }
 
     int GitManager::run_command(const std::string &command, std::string &output) const
@@ -69,24 +75,57 @@ namespace ScheduledGitCommit
         return pipe != nullptr ? 0 : -1;
     }
 
+    std::string GitManager::parse_message() const
+    {
+        std::string msg = message;
+        const auto now = std::chrono::system_clock::now();
+        const auto time = std::chrono::system_clock::to_time_t(now);
+        const auto [tm_sec, tm_min, tm_hour, tm_mday, tm_mon, tm_year, tm_wday, tm_yday, tm_isdst] = *std::localtime(&time);
+        const std::string time_str = std::to_string(tm_hour) + ":" + std::to_string(tm_min) + ":" + std::to_string(tm_sec);
+        const std::string date_str = std::to_string(tm_year + 1900) + "-" + std::to_string(tm_mon + 1) + "-" + std::to_string(tm_mday);
+        const std::string datetime_str = date_str + " " + time_str;
+
+
+        if (const auto pos = msg.find("%t"); pos != std::string::npos)
+        {
+            msg.replace(pos, 2, datetime_str);
+        }
+        if (const auto pos = msg.find("%d"); pos != std::string::npos)
+        {
+            msg.replace(pos, 2, date_str);
+        }
+
+        return msg;
+    }
+
     void GitManager::run() const
     {
         // create a new thread
         std::thread([this]()
         {
             const std::chrono::milliseconds duration(this->time);
-            std::this_thread::sleep_for(duration);
-            if (has_changes())
+            while (true)
             {
-                add_all();
-                commit();
-                if (this->should_create_tag)
+                std::this_thread::sleep_for(duration);
+                try
                 {
-                    create_tag();
-                }
-                if (should_push)
+                    if (has_changes())
+                    {
+                        add_all();
+                        commit();
+                        if (this->should_create_tag)
+                        {
+                            create_tag();
+                        }
+                        if (should_push)
+                        {
+                            push();
+                        }
+                    }
+                } catch (std::exception &e)
                 {
-                    push();
+                    std::cerr << "An error occurred: " << e.what() << std::endl;
+                    break;
                 }
             }
         });
